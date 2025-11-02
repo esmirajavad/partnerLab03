df2ace3d64da5fc5a64bdb0c967fd66bc358e8a1
esmirajavad <esmirababayeva@my.smccd.edu>
2025-11-02 01:06:56 -0700

    Initial Commit

---
+++ partnerLab03/partnerLab03App.swift
@@ -0,0 +1,17 @@
+//
+//  partnerLab03App.swift
+//  partnerLab03
+//
+//  Created by Kamal Aliyev on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct partnerLab03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ partnerLab03Tests/partnerLab03Tests.swift
@@ -0,0 +1,17 @@
+//
+//  partnerLab03Tests.swift
+//  partnerLab03Tests
+//
+//  Created by Kamal Aliyev on 11/2/25.
+//
+
+import Testing
+@testable import partnerLab03
+
+struct partnerLab03Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
